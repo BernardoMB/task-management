b27162ccb1f3da05ace0fccab0fff2176ebfada2
BernardoMB <bmondragonbrozon@gmail.com>
2019-08-20 16:10:08 -0500

    Added authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,16 +4,19 @@ import {
   Body,
   UsePipes,
   ValidationPipe,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  @UsePipes(ValidationPipe)
+  @UsePipes(ValidationPipe) // authCredentialsDto will be validated using decorators inside the AuthCredentialsDto
   signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
@@ -26,7 +29,30 @@ export class AuthController {
   } */
 
   @Post('/signin')
-  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
+  }
+
+  // Guarded endpoint
+
+  /**
+   * This is a guarded endpoint. Because of the AuthGuard,
+   * the function will automaticallyt extract the access token
+   * from the Authorization header of the incoming request.
+   * If the access token is invalid, the AuthGuard will automatically
+   * rejects the request sending an unathorized request status.
+   * If the the request provided a valid access token, the AuthGuard
+   * will inject the user object into que request object
+   * The @Req() decorator gives acces to the complete request object.
+   *
+   * @param {*}
+   * @memberof AuthController
+   */
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@Req() request: any) {
+    console.log(request);
   }
 }
---
+++ src/auth/get-user.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator } from '@nestjs/common';
+import { User } from './user.entity';
+
+export const GetUser = createParamDecorator(
+  (data, request): User => {
+    return request.user;
+  },
+);
