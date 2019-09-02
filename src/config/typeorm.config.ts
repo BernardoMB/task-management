df79987dc5a7aeb084318cf1b785a092df5706e4
BernardoMB <bmondragonbrozon@gmail.com>
2019-09-02 06:46:27 -0500

    Added configuration file

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,17 @@
+import * as config from 'config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+// Import configuration file
+const dbConfig = config.get('db');
 
 // This file has the configuration object for TypeORM inisialization
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '9058',
-  database: 'taskmanagement',
+  type: dbConfig.type,
+  host: dbConfig.host,
+  port: dbConfig.port,
+  username: dbConfig.username,
+  password: dbConfig.password,
+  database: dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   // Everytime the connection starts it is going to syncronize with the schemas in the
   // Postgres database. Should be set to false in production.
