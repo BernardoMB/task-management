7de801d98b1e5d62a0b78b0a2bdef7ad2d2cf59b
BernardoMB <bmondragonbrozon@gmail.com>
2019-09-10 18:49:33 -0500

    Commented main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,25 +11,29 @@ async function bootstrap() {
   // Instanciating the logger class passing bootstrap
   // This logger is being instanciated inside the bootstrap function
   const logger = new Logger('bootstrap');
+
   // Get server confguration.
   // The package config will load the configuration accordingly
   // to the mode in which the application is running on.
   const serverConfig = config.get('server');
   const port = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
-
-  console.log(`Application running on ${process.env.NODE_ENV} environment`);
+  logger.log(`Application running on ${process.env.NODE_ENV} environment`);
 
   // Configure the application to enable CORS in development mode
   if (process.env.NODE_ENV === 'development') {
-    app.enableCors();
+    // Application is in development mode
+    app.enableCors({ origin: '*' });
+    logger.log('Accepting requests from any (*) origin');
   } else {
     // Application is in production mode
     app.enableCors({ origin: serverConfig.origin });
     logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
   }
+
   // Implement application level pipes
   // app.useGlobalPipes(SomePipe);
+
   await app.listen(port);
   // Passing bootstrap because the logger is used inside the bootstrap function.
   logger.log(
